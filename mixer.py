b9c5959bcff2d603b30df665e9deb831578ad659
Bradley H <bhe6001@gmail.com>
2019-07-05 21:06:28 -0400

    Added M-M mixing and fixed issues with interference lasting longer than the target

--- mixer.py
+++ mixer.py
@@ -13,12 +13,16 @@ def tir_factor(ratio, target, interference):
 # TIR = [0, 3, 6, 9, 12, 15]
 
 # for now, I am testing only one specific case
-categories = ['FF']
+categories = ['FF', 'MM']
 TIR = [3]
 dialects = ['DR1', 'DR2', 'DR3', 'DR4', 'DR5', 'DR6', 'DR7']
 
 cbn = sox.Combiner()
 cbn.set_input_format(file_type=['wav', 'wav'])
+
+# trim silence off of end of each audio file
+tfn = sox.Transformer()
+tfn.silence(location=-1)
 
 # create mixed audio files for every combination of categories and TIR
 
@@ -27,6 +31,9 @@ cbn.set_input_format(file_type=['wav', 'wav'])
 
 # shortest duration for test audio: 1.094437
 # longest duration for test audio:: 7.57125
+
+if not os.path.exists("trimmed"):
+	os.makedirs('trimmed')
 
 for ratio in TIR:
 	for category in categories:
@@ -48,42 +55,58 @@ for ratio in TIR:
 
 			lengths = {}
 
-			# populate dict with lengths of audio files
+			# get lengths of audio files
 			if not lengths:
 				for speaker in speakers:
 						for file in os.listdir('timit/TEST/{}/{}'.format(dialect, speaker)):
 							if not file.endswith('.wav'):
 								continue
-							lengths['{}/{}'.format(speaker, file.split('.')[0])] = sox.file_info.duration('timit/TEST/{}/{}/{}'.format(dialect, speaker, file))
+							# get duration of audio file with silence at end trimmed off
+							lengths['{}/{}'.format(speaker, file.split('.')[0])] = tfn.stat('timit/TEST/{}/{}/{}'.format(dialect, speaker, file))['Length (seconds)']
 
-			# mix voice with the voice that is the next smallest duration (to minimize amount of time in target audio that does not contain another voice)
-			sorted_lengths = sorted(lengths.values())
 			sorted_keys = sorted(lengths, key=lengths.get)
 
-			for x in range(len(lengths) - 1, 1, -1):
-				speaker = sorted_keys[x].split('/')[0]
+			for x in range(len(lengths) - 1, 0, -1):
+				speaker1 = sorted_keys[x].split('/')[0]
+				speaker2 = sorted_keys[x - 1].split('/')[0]
 
+				if not os.path.exists('timit/TEST_{}_{}/{}/{}'.format(ratio, category, dialect, speaker1)):
+					os.makedirs('timit/TEST_{}_{}/{}/{}'.format(ratio, category, dialect, speaker1))
+
+				# mix voice with the voice that is the next smallest duration (to minimize amount of time in target audio that does not contain another voice)
 				file1 = 'timit/TEST/{}/{}.wav'.format(dialect, sorted_keys[x])
 				rms1 = sox.file_info.stat(file1)['RMS     amplitude']
-
 				file2 = 'timit/TEST/{}/{}.wav'.format(dialect, sorted_keys[x - 1])
 				rms2 = sox.file_info.stat(file2)['RMS     amplitude']
 
 				factor = tir_factor(ratio, rms1, rms2)
 
-				if not os.path.exists('timit/TEST_{}_{}/{}/{}'.format(ratio, category, dialect, speaker)):
-					os.makedirs('timit/TEST_{}_{}/{}/{}'.format(ratio, category, dialect, speaker))
+				# build trimmed version of interference audio
+				if not os.path.exists('trimmed/{}'.format(speaker2)):
+					os.makedirs('trimmed/{}'.format(speaker2))
 
-				# create audio file and copy over corresponding phoneme, word, and text files
-				cbn.build([file1, file2], 'timit/TEST_{}_{}/{}/{}.wav'.format(ratio, category, dialect, sorted_keys[x]), 'mix', [1, 1 / factor])
+				# although this is computationally expensive, it is required to save the trimmed file in order to mix it with the target.
+				tfn.build('timit/TEST/{}/{}.wav'.format(dialect, sorted_keys[x - 1]), 'trimmed/{}.wav'.format(sorted_keys[x - 1]))
+
+				# mix target audio file and trimmed interference together
+				cbn.build([file1, 'trimmed/{}.wav'.format(sorted_keys[x - 1])], 'timit/TEST_{}_{}/{}/{}.wav'.format(ratio, category, dialect, sorted_keys[x]), 
+										'mix', [1, 1 / factor])
+
+				# copy over corresponding training info
 				shutil.copy('timit/TEST/{}/{}.PHN'.format(dialect, sorted_keys[x]), 
-										'timit/TEST_{}_{}/{}/{}'.format(ratio, category, dialect, speaker))
+										'timit/TEST_{}_{}/{}/{}'.format(ratio, category, dialect, speaker1))
 				shutil.copy('timit/TEST/{}/{}.TXT'.format(dialect, sorted_keys[x]), 
-										'timit/TEST_{}_{}/{}/{}'.format(ratio, category, dialect, speaker))
+										'timit/TEST_{}_{}/{}/{}'.format(ratio, category, dialect, speaker1))
 				shutil.copy('timit/TEST/{}/{}.WRD'.format(dialect, sorted_keys[x]), 
-										'timit/TEST_{}_{}/{}/{}'.format(ratio, category, dialect, speaker))
-				
+										'timit/TEST_{}_{}/{}/{}'.format(ratio, category, dialect, speaker1))
 
+
+				print('Target {} mixed with interf {} with TIR {}'.format(file1, file2, ratio))
+
+				#NOTE: If you comment this line out, SoX will produce warning messages due to files being written over in this directory.
+				shutil.rmtree('trimmed')
+
+				
 # factor = tir_factor(ratio, rms1, rms2)
 # print(calculate_tir(rms1, rms2 / factor))
 # cbn.build(['test/SA1.wav', 'test/SA2.wav'], 'test/SA1_SA2/out{}.wav'.format(ratio), 'mix',  [1, 1 / factor])
